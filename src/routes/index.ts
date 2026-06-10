ccbaf57e30bb31f73a1e905efa2b5add433d76ec
jgbtsuchiya <joaog.tsuchiya@gmail.com>
2026-06-09 22:15:29 -0300

    feat: Produtos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRoutes from './auth.routes';
 import customerRoutes from './customer.routes';
 import merchantRoutes from './merchant.routes';
+import productRoutes from './product.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/customers', customerRoutes);
 router.use('/merchants', merchantRoutes);
+router.use('/products', productRoutes);
 
 export default router;
