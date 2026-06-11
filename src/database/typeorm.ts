58d936fd858683ce95f2cea55c825c8cd29cd14a
Jane Pessoa <100079580+JaneKelli@users.noreply.github.com>
2026-06-11 14:36:11 -0300

    Revert "Feat: Carrinho de Compras"

--- src/database/typeorm.ts
+++ src/database/typeorm.ts
@@ -6,8 +6,6 @@ import { Customer } from '../entities/Cliente';
 import { Product } from '../entities/Produto';
 import { Merchant } from '../entities/Merchant';
 import { PasswordRecovery } from '../entities/PasswordRecovery';
-import { Order } from '../entities/Order';
-import { OrderItem } from '../entities/OrderItem';
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
@@ -18,7 +16,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    entities: [Favorito, Customer, Product, Merchant, PasswordRecovery, Order, OrderItem],
+    entities: [Favorito, Customer, Product, Merchant, PasswordRecovery],
     synchronize: false,
 });
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,12 @@ import customerRoutes from './customer.routes';
 import merchantRoutes from './merchant.routes';
 import productRoutes from './product.routes';
 import favoriteRoutes from './favorite.routes';
-import orderRoutes from './order.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/customers', customerRoutes);
 router.use('/customers/:customerId/favorites', favoriteRoutes);
-router.use('/customers/:customerId/orders', orderRoutes);
 router.use('/merchants', merchantRoutes);
 router.use('/products', productRoutes);
 
